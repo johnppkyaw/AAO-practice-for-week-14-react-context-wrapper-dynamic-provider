19344e0f1f16f86c24b093b7cc963035029ffa12
Pyae (John) Kyaw <jjpyae@gmail.com>
2023-11-16 22:14:59 -0800

    practice complete

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,14 @@
-import { createContext } from 'react';
+import { createContext, useState } from "react";
+import horoscopesObj from "../data/horoscopes.js";
 
 export const HoroscopeContext = createContext();
+
+export const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Libra");
+  const sign = horoscopesObj[currentSign];
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
